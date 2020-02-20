fa241d45512becb91d81ddf0b790c5eb787e64c5
Antonio <radiosabato@hotmail.com>
2020-02-19 22:29:28 -0600

    templates

--- passwordGeneratorProject/generator/views.py
+++ passwordGeneratorProject/generator/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-	return HttpResponse('Hello there')
+	return render(request, 'generator/home.html')
 
-def eggs(request):
-	return HttpResponse('Hello eggs')
+def password(request):
+	return render(request, 'generator/password.html')
