3d8abfa2bb7f7d6484eb5e7103590f39345990de
Antonio Flores Pena <antonio.flores@agilethought.com>
2020-02-21 16:49:06 -0600

    add about template/view

--- passwordGeneratorProject/generator/views.py
+++ passwordGeneratorProject/generator/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # Create your views here.
 def home(request):
 	return render(request, 'generator/home.html')
 
 def password(request):
-	return render(request, 'generator/password.html')
+	thepassword = ''
+	charset = list('abcdefgklmnopqrstuwxyz')
+	length = int(request.GET.get('length'))
+
+	if request.GET.get('uppercase'):
+		charset.extend('ABCDEFGKLMNOPQRSTUWXYZ')
+
+	if request.GET.get('special'):
+		charset.extend('!@#$%^&*()_+><')
+
+	if request.GET.get('numbers'):
+		charset.extend('0123456789')
+
+	for x in range(length):
+		thepassword += random.choice(charset)
+
+	return render(request, 'generator/password.html', {'password': thepassword})
+
+def about(request):
+	return render(request, 'generator/about.html')
