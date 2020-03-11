0941ef2f88fe9e557abed88d0b289db04afbf5fc
Antonio Flores Pena <antonio.flores@agilethought.com>
2020-03-11 17:47:09 -0600

    Home page, and logout method

--- todowooProject/todo/views.py
+++ todowooProject/todo/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.db import IntegrityError
+
+def home(request):
+    return render(request, 'todo/home.html')
 
 def signupuser(request):
     if request.method == 'GET':
@@ -20,6 +23,10 @@ def signupuser(request):
         else:
             return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error': 'Passwords did not match'})
 
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
 
 def currenttodos(request):
     return render(request, 'todo/currenttodos.html')
